c6dc3308f06d33e0a77c20ce333007497fc6b57e
Nicola Salvi <salvinic@users.noreply.github.com>
2026-09-07 07:38:37 +0000

    Make landing page path-independent for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,12 @@
-import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 export default function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="light">
-        <Router />
+        <Home />
       </ThemeProvider>
     </ErrorBoundary>
   );
